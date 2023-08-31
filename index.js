28b824163715ed66a5e5fded3aacaff156fecb29
makaulidz7 <124888138+makaulidz7@users.noreply.github.com>
2023-08-31 09:17:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+tempo = 5000;
+const idade = 18;
+age = 18;
+age = age + 1;
+var timer =setInterval(chamar,tempo);
+
+function chamar(){
+//document.getElement só é chamado com tempo   
+document.getElementById("mostrar").innerHTML = idade;
+document.getElementById("exibir").innerHTML = age;
+}
